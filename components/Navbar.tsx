9860c0576c01bec240f8c4fa908cf2f7e71af5a7
aamiryounis105 <aamiryounis105@gmail.com>
2025-12-14 00:02:55 +0500

    Remove unused icons from Navbar

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -2,7 +2,7 @@
 
 import { useState, useEffect } from "react";
 import Link from "next/link";
-import { ArrowUpRight, Moon, Sun } from "lucide-react";
+import { ArrowUpRight } from "lucide-react";
 import { usePathname } from "next/navigation";
 
 export default function Navbar() {
